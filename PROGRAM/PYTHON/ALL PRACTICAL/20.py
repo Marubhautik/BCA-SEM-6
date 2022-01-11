8efe68add8967589b8aed0c7bf97e6ddb569539d
MArubhautik <marubhautik97@gmail.com>
2022-01-11 15:39:57 +0530

    program

---
+++ PROGRAM/PYTHON/ALL PRACTICAL/20.py
@@ -0,0 +1,9 @@
+#WAP for display ASCII value
+val = input("Enter a number")
+
+print("ASCII value of ",val," is ",ord(val))
+'''
+Enter a numberb
+ASCII value of  b  is  98
+'''
+
---
+++ PROGRAM/PYTHON/ALL PRACTICAL/21.py
@@ -0,0 +1,9 @@
+#WAP for cube sum of first n natural numbers
+num = int(input("Enter Number"))
+sum1 =0
+for n in range(1,num+1):
+    sum1 = (n*n*n)
+print(sum1)
+'''
+125
+'''
